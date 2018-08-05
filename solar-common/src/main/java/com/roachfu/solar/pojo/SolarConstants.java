5b0487fc27bb05f5eee940f55ccf084a2224ae38
roachfu <fuqiang.it@gmail.com>
2018-08-05 17:42:08 +0800

    feature(#solar-common): 新增全局常量类

---
+++ solar-common/src/main/java/com/roachfu/solar/pojo/SolarConstants.java
@@ -0,0 +1,23 @@
+package com.roachfu.solar.pojo;
+
+/**
+ * 全局常量
+ *
+ * @author roach
+ * @date 2018/8/5 15:38
+ */
+public class SolarConstants {
+
+    /**
+     * 斜杠
+     */
+    public static final String SLASH = "/";
+
+    /**
+     * utf-8
+     */
+    public static final String UTF8 = "UTF-8";
+
+    private SolarConstants() {
+    }
+}
