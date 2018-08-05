b2548b72278bed33fa3fcd3c92db1ce69eaf0b0d
roachfu <fuqiang.it@gmail.com>
2018-08-05 17:43:41 +0800

    refactor(#solar-common): 删除无用方法

--- solar-common/src/main/java/com/roachfu/solar/util/property/PropertiesUtils.java
+++ solar-common/src/main/java/com/roachfu/solar/util/property/PropertiesUtils.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.Map.Entry;
 import java.util.Properties;
 
 /**
@@ -44,6 +41,7 @@ public class PropertiesUtils {
 
     /**
      * 获取配置属性
+     *
      * @param key 键
      * @return 值
      */
@@ -54,70 +52,11 @@ public class PropertiesUtils {
     /**
      * 获取配置属性，如果为空则赋予默认值
      *
-     * @param key 键
+     * @param key          键
      * @param defaultValue 默认值
      * @return 值
      */
-    public String getProperty(String key, String defaultValue){
+    public String getProperty(String key, String defaultValue) {
         return properties.getProperty(key, defaultValue);
-    }
-
-    /**
-     * 遍历properties, propertyNames()方法的使用
-     *
-     * @param properties
-     */
-    @SuppressWarnings("unchecked")
-    public void forEach_1(Properties properties) {
-        Enumeration<String> keyEnum = (Enumeration<String>) properties.propertyNames();
-        while (keyEnum.hasMoreElements()) {
-            String key = keyEnum.nextElement();
-            String value = properties.getProperty(key);
-            System.out.println(key + " = " + value);
-        }
-    }
-
-    /**
-     * 遍历properties, stringPropertyNames()方法的使用
-     *
-     * @param properties
-     */
-    public void forEach_2(Properties properties) {
-        Iterator<String> iterator = properties.stringPropertyNames().iterator();
-        while (iterator.hasNext()) {
-            String key = iterator.next();
-            String value = properties.getProperty(key);
-            System.out.println(key + " = " + value);
-        }
-    }
-
-    /**
-     * 遍历properties, entrySet()方法的使用
-     *
-     * @param properties
-     */
-    public void forEach_3(Properties properties) {
-        Iterator<Entry<Object, Object>> entrySet = properties.entrySet().iterator();
-        while (entrySet.hasNext()) {
-            Entry<Object, Object> entry = (Entry<Object, Object>) entrySet.next();
-            String key = (String) entry.getKey();
-            String value = (String) entry.getValue();
-            System.out.println(key + " = " + value);
-        }
-
-    }
-
-
-    /**
-     * 获取properties下所有的values
-     *
-     * @param properties
-     */
-    public void getValues(Properties properties) {
-        Enumeration<Object> keyEnum = properties.elements();
-        while (keyEnum.hasMoreElements()) {
-            String value = (String) keyEnum.nextElement();
-            System.out.println(value);
-        }
     }
 }
